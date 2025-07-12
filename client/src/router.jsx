7fe1c1e5b3f85d67417d384e1dd23681be503dfd
tjthecalculator <mr.tawatchai@live.com>
2025-07-12 23:41:44 +0700

    add more file and code

---
+++ client/src/router.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+
+const Router = () => {
+
+    const location = useLocation();
+
+    return (
+        <Routes>
+            {}
+        </Routes>
+    );
+};
+
+export default Router;
